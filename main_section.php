59ab054307dea7648c919c5df30c4e4e3065f033
Louinis BOUDAA <lboudaa@e3r13p2.42.fr>
2018-03-17 05:11:36 +0100

    yeugh

--- main_section.php
+++ main_section.php
@@ -201,10 +201,16 @@ function merge_images($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, 
           var data = canvas.toDataURL('image/png');
           photo.setAttribute('src', data);
           capture.setAttribute('value', data);
-          document.location.reload(true);
         }
 
         startbutton.addEventListener('click', function(ev){
+            console.log("in");
+            document.getElementById("startbutton").className = "make-background-grey"; 
+            document.getElementById("startbutton").disabled = true;
+            document.getElementById('box1').checked == false;
+            document.getElementById('box2').checked == false;
+            document.getElementById('box3').checked == false;
+            document.getElementById('box4').checked == false;
             takepicture();
         }, false);
 
